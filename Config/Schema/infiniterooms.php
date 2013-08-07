51e755d7422c5cc55057303c01d79cfd97f3e702
James Ballard <jimbonda@gmail.com>
2013-08-07 17:58:18 +0700

    #23 - fix key in schema

--- Config/Schema/infiniterooms.php
+++ Config/Schema/infiniterooms.php
@@ -118,7 +118,7 @@ class infiniteroomsSchema extends CakeSchema {
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1),
-			'idnumber' => array('column' => 'sysid', 'unique' => 1),
+			'idnumber' => array('column' => 'sysname', 'unique' => 1),
 			'name' => array('column' => 'name', 'unique' => 1)
 		),
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
