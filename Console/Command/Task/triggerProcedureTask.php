0936cf62e25f64544fabd2d1a8a26e1eb049ccfe
James Ballard <jimbonda@gmail.com>
2013-10-22 22:01:18 +0100

    #36 - Add start and end IDs to the cron process for users and groups

--- Console/Command/Task/triggerProcedureTask.php
+++ Console/Command/Task/triggerProcedureTask.php
@@ -1,6 +1,6 @@
 <?php
 class TriggerProcedureTask extends Shell {
-    public $uses = array('Customer', 'Rule', 'Action', 'CustomerStatus');
+    public $uses = array('Customer', 'Rule', 'Action', 'Group', 'User', 'CustomerStatus');
     public function execute() {
         $customers = $this->Customer->find('all', array(
             'conditions' => array(
@@ -9,60 +9,122 @@ class TriggerProcedureTask extends Shell {
         ));
         foreach ($customers as $customer) {
             $customerid = $customer['Customer']['id'];
-            $types = $this->Rule->rule_types;
-            foreach ($types as $typekey=>$type) {
-                $rules = $this->Rule->getCustomerRules($customerid);
-                foreach ($rules as $rule) {
-                    $ruleid = $rule['Rule']['id'];
-                    $lastid = $this->CustomerStatus->find('first', array(
-                            'conditions' => array(
-                                'type' => $typekey,
-                                'rule_id' => $ruleid,
-                                'customer_id' => $customerid
-                            ),
-                            'fields' => array('endid')
-                        )
-                    );
-                    $maxid = $this->Action->find('first', array(
-                            'contain' => array(
-                                'System'
-                            ),
-                            'conditions' => array(
-                                'System.customer_id' => $customerid
-                            ),
-                            'fields' => array('MAX(Action.id) as maxid'),
-                        )
-                    );
+            // Find the latest group for the customer.
+            $maxid = $this->User->find('first', array(
+                    'contain' => array(
+                        'System'
+                    ),
+                    'conditions' => array(
+                        'System.customer_id' => $customerid
+                    ),
+                    'fields' => array('MAX(User.id) as maxid'),
+                )
+            );
+            //Define the latest group mapping id for the customer
+            $maxid = (int)$maxid[0]['maxid'];
 
-                    //Define the latest action id for the customer
-                    $maxid = (int)$maxid[0]['maxid'];
+            $lastid = $this->CustomerStatus->find('first', array(
+                    'conditions' => array(
+                        'type' => CustomerStatus::PROC_USER_TO_PEOPLE,
+                        'customer_id' => $customerid
+                    ),
+                    'fields' => array('endid')
+                )
+            );
 
-                    //Define the last id processed for the customer
-                    $lastid = (empty($lastid) ? 0 : (int)$lastid['CustomerStatus']['endid']);
+            //Define the last id processed for the customer
+            $lastid = (empty($lastid) ? 0 : (int)$lastid['CustomerStatus']['endid']);
 
-                    //Compare values and only process if new actions exist since last procedure update
-                    if($maxid > $lastid) {
-                        //Call process based on the type
-                        switch($typekey) {
-                            case 1:
-                                $this->CustomerStatus->query("CALL map_users_to_person($customerid);");
-                                break;
-                            case 2:
-                                $this->CustomerStatus->query("CALL map_groups_to_course($customerid);");
-                                break;
-                            case 3:
-                                $this->CustomerStatus->query("CALL aggregate_summed_actions($customerid, $lastid, $maxid);");
-                                break;
-                            case 4:
-                                $this->CustomerStatus->query("CALL aggregate_summed_rule_conditions($customerid, $lastid, $maxid, $ruleid);");
-                                break;
-                            case 5:
-                                //$this->CustomerStatus->query("CALL aggregate_summed_ip_conditions($customerid, $lastid, $maxid, 2);");
-                                break;
-                        }
-                    }
+            if($maxid > $lastid) {
+                $this->CustomerStatus->query("CALL map_users_to_person($customerid, $lastid, $maxid);");
+            }
+
+            // Find the latest group for the customer.
+            $maxid = $this->Group->find('first', array(
+                    'contain' => array(
+                        'System'
+                    ),
+                    'conditions' => array(
+                        'System.customer_id' => $customerid
+                    ),
+                    'fields' => array('MAX(Group.id) as maxid'),
+                )
+            );
+            //Define the latest group mapping id for the customer
+            $maxid = (int)$maxid[0]['maxid'];
+
+            $lastid = $this->CustomerStatus->find('first', array(
+                    'conditions' => array(
+                        'type' => CustomerStatus::PROC_GROUP_TO_COURSE,
+                        'customer_id' => $customerid
+                    ),
+                    'fields' => array('endid')
+                )
+            );
+
+            //Define the last id processed for the customer
+            $lastid = (empty($lastid) ? 0 : (int)$lastid['CustomerStatus']['endid']);
+
+            if($maxid > $lastid) {
+                $this->CustomerStatus->query("CALL map_groups_to_course($customerid, $lastid, $maxid);");
+            }
+
+            // Find the latest action for the customer.
+            $maxid = $this->Action->find('first', array(
+                    'contain' => array(
+                        'System'
+                    ),
+                    'conditions' => array(
+                        'System.customer_id' => $customerid
+                    ),
+                    'fields' => array('MAX(Action.id) as maxid'),
+                )
+            );
+            //Define the latest sum action id for the customer
+            $maxid = (int)$maxid[0]['maxid'];
+
+            $lastid = $this->CustomerStatus->find('first', array(
+                    'conditions' => array(
+                        'type' => CustomerStatus::PROC_SUM_ACTION,
+                        'customer_id' => $customerid
+                    ),
+                    'fields' => array('endid')
+                )
+            );
+
+            //Define the last id processed for the customer
+            $lastid = (empty($lastid) ? 0 : (int)$lastid['CustomerStatus']['endid']);
+
+            if($maxid > $lastid) {
+                $this->CustomerStatus->query("CALL aggregate_summed_actions($customerid, $lastid, $maxid);");
+            }
+
+            $rules = $this->Rule->getCustomerRules($customerid);
+            foreach ($rules as $rule) {
+                if ($rule['Rule']['name'] == 'IP Address') {
+                    // Don't process this.
+                    continue;
+                }
+                $ruleid = $rule['Rule']['id'];
+                $lastid = $this->CustomerStatus->find('first', array(
+                        'conditions' => array(
+                            'type' => CustomerStatus::PROC_SUM_RULE,
+                            'rule_id' => $ruleid,
+                            'customer_id' => $customerid
+                        ),
+                        'fields' => array('endid')
+                    )
+                );
+
+                //Define the last id processed for the customer
+                $lastid = (empty($lastid) ? 0 : (int)$lastid['CustomerStatus']['endid']);
+
+                //Compare values and only process if new actions exist since last procedure update
+                if($maxid > $lastid) {
+                    $this->CustomerStatus->query("CALL aggregate_summed_rule_conditions($customerid, $lastid, $maxid, $ruleid);");
                 }
             }
+            //$this->CustomerStatus->query("CALL aggregate_summed_ip_conditions($customerid, $lastid, $maxid, 2);");
         }
     }
 }
