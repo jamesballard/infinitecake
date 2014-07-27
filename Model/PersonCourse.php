bdd8a90997d90456dfa9063c287d4a1136c2fad9
James Ballard <jimbonda@gmail.com>
2014-07-27 20:00:13 +1000

    Remove settings n PersonCourse

--- Model/PersonCourse.php
+++ Model/PersonCourse.php
@@ -9,17 +9,6 @@ App::uses('AppModel', 'Model');
 class PersonCourse extends AppModel {
 
 /**
- * Display field
- *
- * @var string
- */
-	public $displayField = 'name';
-
-
-    public $useTable = 'person_courses';
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
-
-/**
  * belongsTo associations
  *
  * @var array
